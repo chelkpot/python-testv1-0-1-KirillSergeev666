6310ca94251e274c15075c1a39f0bcc4b2ca1c9f
Kirill <cyrills3rgeeff@yandex.ru>
2025-12-18 02:12:27 +0500

    k2

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,7 +4,11 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
-
+    # Задание 5. Квадраты чисел (map)
+    numbers = input("Введите числа через пробел: ")
+    numbers_list = list(map(int, numbers.split()))
+    squares = list(map(lambda x: x ** 2, numbers_list))
+    print("Результат:", " ".join(map(str, squares)))
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
